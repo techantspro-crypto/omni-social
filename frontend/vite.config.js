fe7bca64b865ee9a00aa735364e812785c15523c
Luke <techantspro@gmail.com>
2025-11-28 10:26:01 +0800

    Add files via upload

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // [最終修正] 修正模組解析問題
+  resolve: {
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.css']
+  }
 })
